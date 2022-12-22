0babd5492aad575ce051b63c8637f54bf392d2e7
Mohammed Hassan <medo.hassan0100@gmail.com>
2022-12-22 17:51:07 +0200

    start day

--- database/migrations/2022_12_22_143051_create_countries_table.php
+++ database/migrations/2022_12_22_143051_create_countries_table.php
@@ -13,16 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('number');
-            $table->string('email')->unique();
-            $table->string('userType')->default('user');
-            $table->string('verification_code')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -34,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('countries');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CountryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/send_code', [AuthController::class, 'send_code']);
+Route::post('/confirm_code/{id}', [AuthController::class, 'confirm_code']);
+Route::post('password_reset', [AuthController::class, 'password_reset']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/editData', [AuthController::class, 'editData']);
+    Route::post('/change_password', [AuthController::class, 'change_password']);
+});
+
+Route::get('/countries', [CountryController::class, 'index']);
